026f859005b9aac3633baf4e5c3131598309695d
iivanov791 <55314229+iivanov791@users.noreply.github.com>
2019-12-04 15:24:16 +0200

    Update index.js

--- ListWrapper/index.js
+++ ListWrapper/index.js
@@ -6,26 +6,44 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import {useMutation} from '@apollo/react-hooks';
+import {FollowMutation} from "../../queries/mutation";
+
+
 
 
 export default function ListAll (props) {
 
-        const followings = props.users[0].following;
+
+    const currentUserId = props.users[0].id;
+
+    const followings = props.users[0].following;
+
+    const [sendFollow] = useMutation(FollowMutation);
 
 
-
-        function handleClick (e) {
-
-            e.target.innerHTML === 'Unfollow'?e.target.innerHTML = 'Follow':e.target.innerHTML = 'Unfollow';
+    async function handleClick (e) {
+            e.preventDefault();
+            e.persist();
+           try {
+               const response = await sendFollow({
+                   variables: {user: currentUserId, toFollow: e.target.parentNode.id}
+               });
+               const result = response.data.follow;
+             
+               result === true?e.target.innerText = 'Unfollow':e.target.innerText = 'Follow';
+           } catch(err) {
+               console.log(err);
+           }
         }
 
         return (
 
             <List >
 
-                {props.users.map(data => {
+                {props.users.map((data,i) => {
                             return (
-                                <ListItem key={data.id}>
+                                <ListItem key={i}>
                                     <ListItemAvatar>
                                         <Avatar
                                             alt={`Avatar`}
@@ -33,18 +51,18 @@ export default function ListAll (props) {
                                         />
                                     </ListItemAvatar>
                                     <ListItemText primary={data.username} />
-                                    {followings.map(item=> {
+                                    {followings.map( (item,i )=> {
                                         if (item.username === data.username) {
                                             return (
-                                                <ListItemSecondaryAction>
-                                                    <Button color="primary" onClick={handleClick}>Unfollow</Button>
+                                                <ListItemSecondaryAction key={i}>
+                                                    <Button color="primary" onClick={handleClick} id={data.id}>Unfollow</Button>
                                                 </ListItemSecondaryAction>
                                             )
                                         }
                                         if(item.username!==data.username) {
                                             return(
-                                                <ListItemSecondaryAction>
-                                                    <Button color="primary" onClick={handleClick}>Follow</Button>
+                                                <ListItemSecondaryAction key={i}>
+                                                    <Button color="primary" onClick={handleClick} id={data.id}>Follow</Button>
                                                 </ListItemSecondaryAction>
                                             )
                                         }
